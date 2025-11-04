ee5318129556a16160135df7b4e0b40d66a7406a
AlexandraCMB <alexandra.brunjes@gmail.com>
2025-11-04 13:09:23 +0000

    little error woops

--- ft_isprint.c
+++ ft_isprint.c
@@ -3,38 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abrunjes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: abrunjes <abrunjes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 10:58:50 by abrunjes          #+#    #+#             */
-/*   Updated: 2025/10/26 11:41:33 by abrunjes         ###   ########.fr       */
+/*   Updated: 2025/11/04 12:57:45 by abrunjes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isprint(int c)
+int ft_isprint(int c)
 {
-	if (c > 32 && c < 127)
+	if (c > 31 && c < 127)
 		return (1);
 	else
 		return (0);
 }
-/*
-#include <stdio.h>
-
-int main (void)
-{
-	int a = 's';
-	int b = 'S';
-	int c = '*';
-	int d = 1;
-	int e = '7';
-	printf("Yes = 1, No = 0\n");
-	printf(" Is %c alpha?:%d \n", a, ft_isprint(a));
-	printf(" Is %c alpha?:%d \n", b, ft_isprint(b));
-	printf(" Is %c alpha?:%d \n", c, ft_isprint(c));
-	printf(" Is %c alpha?:%d \n", d, ft_isprint(d));
-	printf(" Is %c alpha?:%d \n", e, ft_isprint(e));
-	return 0;
-}
-*/
