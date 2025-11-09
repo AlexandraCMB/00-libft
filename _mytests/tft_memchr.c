0c1ae15eac20a987e5bea2c265be0418a7174885
AlexandraCMB <alexandra.brunjes@gmail.com>
2025-11-09 18:44:41 +0000

    i hate fireworks

--- _mytests/tft_memchr.c
+++ _mytests/tft_memchr.c
@@ -1,35 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   tft_memchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abrunjes <abrunjes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 18:58:30 by abrunjes          #+#    #+#             */
-/*   Updated: 2025/11/09 18:39:36 by abrunjes         ###   ########.fr       */
+/*   Updated: 2025/11/09 18:38:16 by abrunjes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+int main(void)
 {
-	const unsigned char	*str;
-	unsigned char		find;
 
-	str = (unsigned char *)s;
-	find = (char )c;
-	while (n--)
-	{
-		if (*str == find)
-			return ((void *)str);
-		str++;
-	}
-	return (NULL);
+	char str[] = {"AlphaBetaGamma"};
+	int s = 'B';
+	size_t n = 9;
+	char *ans = ft_memchr(str, s, n);
+	printf("1) Finding %c in %s returns the pointer\n", s, str);
+	printf("to %s which is %p\n", ans, ans);
+
+	int strstr[7] = {-49, 49, 1, -1, 0, -2, 2};
+	int x = -1;
+	size_t y = 7;
+	void *ans2 = ft_memchr(strstr, x, y);
+	printf("FT_M) Finding %d in strstr returns\n", x);
+	printf(" the pointer %p.\n", ans2);
+	void *ans3 = memchr(strstr, x, y);
+	printf("M) Finding %d in strstr returns\n", x);
+	printf(" the pointer %p.\n", ans3);
+	return (0);
 }
+
 /* DESCRIPTION
-The  memchr()  function scans the initial n bytes of the memory area pointed 
-to by s for the first instance of c. Both c and the bytes of the memory area 
+The  memchr()  function scans the initial n bytes of the memory area pointed
+to by s for the first instance of c. Both c and the bytes of the memory area
 pointed to by s are interpreted as unsigned char.
 
 char	*ft_strchr(const char *s, int c)
